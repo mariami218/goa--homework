a57918727af362387c9fb89d3ed2b10fbfdb679b
mariami218 <mariamiberishvili85@gmail.com>
2024-12-22 06:35:06 -0800

    commit

---
+++ level31/homework/index.py
@@ -0,0 +1,13 @@
+def greet():
+    return "hello world!"
+
+# Example usage
+print(greet())  # Outputs: hello world!
+
+def opposite(number):
+    return -number
+
+# Example usage
+print(opposite(1))    # Outputs: -1
+print(opposite(14))   # Outputs: -14
+print(opposite(-34))  # Outputs: 34
